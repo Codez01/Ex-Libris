378ba00421db79c5444e57c856e39da726650dc5
Codez01 <80861363+Codez01@users.noreply.github.com>
2022-01-27 22:38:02 +0200

    Update CodeFetcher.js

--- Incidents Sorter/CodeFetcher.js
+++ Incidents Sorter/CodeFetcher.js
@@ -1,1 +1,1 @@
-javascript:var code;fetch("https://raw.githubusercontent.com/Codez01/Ex-Libris/main/Incidents Sorter/LatestSummary.js").then(e=>e.text()).then(e=>code=e),eval(code);var js=code,oScript=document.createElement("script"),oScriptText=document.createTextNode(js);oScript.appendChild(oScriptText);
+var code;fetch("https://raw.githubusercontent.com/Codez01/Ex-Libris/main/Incidents Sorter/LatestSummary.js").then(e=>e.text()).then(e=>code=e),eval(code);var js=code,oScript=document.createElement("script"),oScriptText=document.createTextNode(js);oScript.appendChild(oScriptText);
